a6201484105c76c87816dbf3d030c5e5bc9365af
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 11:36:48 +0000

    Adding tarfile member sanitization to extractall()

--- docker.py
+++ docker.py
@@ -76,7 +76,26 @@ def main(tarball, install_ius, uninstall_ius):
             pathlib.Path(member.name).parts[0]
             for member in tarball.getmembers()
         }
-        tarball.extractall(path=installer)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarball, path=installer)
 
     setup, = (installer/base).glob('ccs_setup_*.bin')
 
